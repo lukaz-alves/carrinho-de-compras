3c8f7525f360dc9fb3db52f298a5b17737b74282
Lucas Alves <lucaz.santos1@outlook.com>
2026-07-20 17:31:45 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const carrinho = [];
+
+carrinho.push({id: 1, nome: "Arroz", preco: 20.00, quantidade: 2});
+carrinho.push({id: 2, nome: "Feijão", preco: 6.50, quantidade: 4});
+carrinho.push({id: 3, nome: "Açucar", preco: 3.20, quantidade: 5});
+
+console.log(carrinho);
